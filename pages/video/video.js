a3f9e8045118bf2ca1dd46282521394f031221c7
Razzh <136013779@qq.com>
2021-10-13 23:19:11 +0800

    add:视频页导航栏搭建

--- pages/video/video.js
+++ pages/video/video.js
@@ -1,20 +1,32 @@
-// pages/video/video.js
+import request from "../../http/http";
 Page({
 
     /**
      * 页面的初始数据
      */
     data: {
-
+        videoTag: [],
+        tagId: ''
     },
-
+    currTag(e) {
+        let tagId = e.target.dataset.id;
+        console.log(typeof this.data.tagId)
+        this.setData({
+            tagId
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        request('/video/group/list').then(res => {
+            this.setData({
+                videoTag: res.data.slice(0,14),
+                tagId: res.data.slice(0,14)[0].id // 拿到第一个数组id以便初次渲染激活第一个tag的active
+            })
+        })
     },
-
+    
     /**
      * 生命周期函数--监听页面初次渲染完成
      */
