a800e913be518188d67fe281547f8004243f525c
Razzh <136013779@qq.com>
2021-10-13 19:59:02 +0800

    add:完成登录功能

---
+++ pages/login/login.js
@@ -0,0 +1,109 @@
+// pages/login/login.js
+import request from "../../http/http";
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        phone: '',
+        password: ''
+    },
+    handleInput(e) {
+        let type = e.target.dataset.type,
+            value = e.detail.value;
+        this.setData({
+            [type]: value  // 向phone和password传递input输入的值
+        })
+    },
+    login() {
+        let {phone, password} = this.data,
+            phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/,
+            passwordReg = /^[a-z0-9_-]{6,18}$/;
+        if(!phone) { // 验证手机号合法性
+            wx.showToast({
+                title: '手机号不能为空',
+                icon: 'error'
+            })
+        } else if(!phoneReg.test(phone)) {
+            wx.showToast({
+                title: '手机号格式错误',
+                icon: 'error'
+            })
+        }
+        if(!passwordReg.test(password)) { // 校验密码是否正确
+            wx.showToast({
+              title: '密码格式不正确',
+              icon: 'error'
+            })
+        }
+        // 发送请求
+        request('/login/cellphone',{phone,password}).then(res => {
+            wx.setStorageSync('userInfo', JSON.stringify(res.profile));
+            wx.showToast({
+                title: '正在登陆',
+                icon: 'loading',
+                success: () => {
+                    wx.reLaunch({
+                      url: '/pages/personal/personal',
+                    })
+                }
+              })
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
---
+++ pages/personal/personal.js
@@ -0,0 +1,113 @@
+import request from "../../http/http";
+let startY,moveY,resY;
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        translate: 'translateY(0)',
+        transition: '',
+        userInfo: {},
+        recentPlayList: []
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    handleTouchStart(e) {
+        startY = e.touches[0].clientY; //获取开始鼠标距离窗口顶部的Y坐标
+    },
+    handleTouchMove(e) {
+        moveY = e.touches[0].clientY; //获取开始鼠标距离窗口顶部的Y坐标
+        resY = moveY - startY;
+        if(resY < 0) {
+            return;
+        }
+        if(resY >= 80) {
+            resY = 80;
+        }
+        this.setData({
+            translate: `translateY(${resY}rpx)`,
+            transition: 'transfrom 1s linear'
+           })
+    },
+    handleTouchEnd(e) {
+        this.setData({
+            translate: `translateY(0rpx)`,
+            transition: 'transfrom 1s linear'
+           })
+    },
+    toLogin() {
+        wx.redirectTo({
+          url: '/pages/login/login'
+        })
+    },
+    getRecord(id) { //获取历史播放记录
+        request('/user/record',{uid: id, type: 1}).then(res => {
+            let index = 0;
+            let result = res.weekData.slice(0,10).map(elem => {
+                elem.id = index++;
+                return elem;
+            })
+            this.setData({
+                recentPlayList: result
+            })
+        })
+    },
+    onLoad: function (options) {
+        this.setData({ //保存userInfo数据到data
+            userInfo: JSON.parse(wx.getStorageSync('userInfo')) //反序列化取出Storage中的用户信息
+        })
+        this.getRecord(this.data.userInfo.userId);
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function () {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function () {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function () {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function () {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
